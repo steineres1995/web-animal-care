f73b6e7e6f1c306d8b45925f04d5b75abf603f9f
Esteban Montes de Oca <steineres1995@live.com>
2023-09-25 23:01:27 -0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import { Login } from './components/Login/Login';
+import { Dashboard } from './components/Dashboard/Dashboard';
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World!!</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />}></Route>
+      <Route path="test" element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
