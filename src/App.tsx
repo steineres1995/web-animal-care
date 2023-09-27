033ae2e7ff7a81b2b1a970aa670b0256e2fecc11
Esteban Montes de Oca <steineres1995@live.com>
2023-09-26 23:08:47 -0300

    Added new routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Login } from './components/Login/Login';
 import { Dashboard } from './components/Dashboard/Dashboard';
+import { CreateAnimal } from './components/CreateAnimal/CreateAnimal';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />}></Route>
-      <Route path="test" element={<Dashboard />} />
+      <Route path="dashboard" element={<Dashboard />} />
+      <Route path="animal" element={<CreateAnimal />} />
     </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,16 @@ import App from './App.tsx';
 import './index.css';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
-);
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+} else {
+  console.error('El elemento con id "root" no se encontró en el DOM.');
+}
