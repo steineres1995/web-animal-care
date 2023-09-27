005386338ca3687f059009a2607256b6c4aca55f
Esteban Montes de Oca <steineres1995@live.com>
2023-09-26 23:10:22 -0300

    Added styles to Button component

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -4,5 +4,12 @@ interface ButtonProps {
 }
 
 export const Button = ({ text, onClick }: ButtonProps) => {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <button
+      className="w-ful bg-blue-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 };
