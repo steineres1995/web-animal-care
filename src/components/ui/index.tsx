d413f296016e52e28cf25ad65835a0b782a9ab70
Esteban Montes de Oca <steineres1995@live.com>
2023-09-26 23:09:40 -0300

    Added to UI index Select and Container component

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,4 +1,6 @@
 import { Button } from './Button/Button';
 import { Input } from './Input/Input';
+import { Select } from './Select/Select';
+import { Container } from './Container/Container';
 
-export { Button, Input };
+export { Button, Input, Select, Container };
