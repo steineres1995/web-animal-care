a8d4feaf54678d4db58150dc54963f6f6dd4242a
Esteban Montes de Oca <steineres1995@live.com>
2023-09-26 23:10:09 -0300

    Added styles to input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,15 +1,26 @@
 interface InputProps {
-	label: string;
-	type: string;
-	name: string;
-	value: string;
-	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  type: string;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 export const Input = ({ label, type, name, value, onChange }: InputProps) => {
-	return (
-		<div>
-			<label htmlFor={name}>{label}</label>
-			<input type={type} name={name} value={value} onChange={onChange} />
-		</div>
-	)
-}
+  return (
+    <div>
+      <label
+        className="block mb-2 text-sm font-medium text-gray-900"
+        htmlFor={name}
+      >
+        {label}
+      </label>
+      <input
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
